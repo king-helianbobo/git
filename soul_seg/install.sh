90184d82f1aa569128037c114b259e49a21d83fd
helianbobo1983 <105323647@qq.com>
2014-01-03 20:16:32 +0800

    merge

--- soul_seg/install.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-mvn clean compile
-mvn assembly:single
-/opt/elasticsearch-0.90.5/bin/plugin --remove suggest
-/opt/elasticsearch-0.90.5/bin/plugin --url file:///home/liubo/git/soul_seg/target/releases/soul_seg-0.2-plugin.zip  --install suggest
-
