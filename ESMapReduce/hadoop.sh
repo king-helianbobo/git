5debe70b123bc06d0b986c4798063455ec716ea5
helianbobo1983 <105323647@qq.com>
2014-01-12 10:26:05 +0800

    delete mapreduce

--- ESMapReduce/hadoop.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash                                                                                                 
-rm -f es.jar
-hadoop fs -copyToLocal hdfs://192.168.50.75:9000/user/liubo/1.jar ./es.jar
-hadoop fs -rm -r  /liubo_output
-hadoop jar es.jar /chenlou_input /liubo_output
