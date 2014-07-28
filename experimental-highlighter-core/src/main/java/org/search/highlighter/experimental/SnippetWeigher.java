56639ae5fa8e5bb39beb579ee96666036d3d72bf
liubo <bo.liu@soulinfo.com>
2014-07-28 09:32:20 +0800

    test

--- experimental-highlighter-core/src/main/java/org/search/highlighter/experimental/SnippetWeigher.java
+++
@@ -1,9 +0,0 @@
-package org.search.highlighter.experimental;
-
-import java.util.List;
-
-import org.search.highlighter.experimental.Snippet.Hit;
-
-public interface SnippetWeigher {
-    float weigh(List<Hit> hits);
-}
--- experimental-highlighter-core/src/main/java/org/search/highlighter/experimental/hit/TermSourceFinder.java
+++
@@ -1,9 +0,0 @@
-package org.search.highlighter.experimental.hit;
-
-
-/**
- * Finds the source of the hit based on the term.
- */
-public interface TermSourceFinder<T> {
-    int source(T term);
-}
