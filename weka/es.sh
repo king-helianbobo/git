d685c08125233dae2be50980b79c8d357e1dc8cd
king-helianbobo <lau19831003@aliyun.com>
2014-11-25 20:13:58 +0800

    test

--- weka/es.sh
+++
@@ -1,6 +0,0 @@
-{"index" : "tax_test", "type" : "A01"}
-{"query" : {"match_all": {}}, "from":0,"size":10}
-{"index" : "tax_test", "type" : "A02"}
-{"query" : {"match_all": {}}, "from":0,"size":10}
-{"index" : "tax_test", "type" : "A03"}
-{"query" : {"match_all": {}}, "from":0,"size":10}
